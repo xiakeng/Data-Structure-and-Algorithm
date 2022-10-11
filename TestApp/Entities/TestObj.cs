9110fcc636c4e31d4b40431c186aeb579b311462
xiakeng <xiakeng@outlook.com>
2022-10-11 20:44:43 +0800

    solution initial

---
+++ TestApp/Entities/TestObj.cs
@@ -0,0 +1,11 @@
+using System;
+namespace TestApp.Entities;
+
+public class TestObj
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public DateTime Modified { get; set; }
+}
---
+++ TestLib/JsonUtils.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+namespace TestLib;
+
+public static class JsonUtils
+{
+    public static string ToJson(object obj)
+    {
+        return JsonConvert.SerializeObject(obj, Formatting.Indented);
+    }
+    
+    public static T FromJson<T>(string json)
+    {
+        return JsonConvert.DeserializeObject<T>(json);
+    }
+}
