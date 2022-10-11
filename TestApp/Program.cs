ca7ae2abbbf707bd7e4244fb81bf30fc5c84215d
xiakeng <xiakeng@outlook.com>
2022-10-11 20:48:54 +0800

    solution initial (#1)

---
+++ TestApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using TestApp.Entities;
+using TestLib;
+namespace TestApp;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var obj = new TestObj()
+        {
+            Id = 1,
+            Name = "aaa",
+            Modified = DateTime.Now
+        };
+
+        var json = JsonUtils.ToJson(obj);
+
+        Console.WriteLine(json);
+
+        var fromJson = JsonUtils.FromJson<TestObj>(json);
+
+        Console.ReadKey();
+    }
+}
